38aa2b3e8ab195a5bd845debee623c2f77662ea5
Shiraishi-Sayuki <sayukishiraishi@gmail.com>
2026-03-31 13:46:20 +0900

    Fix public API TODO stubs for ship lookup helpers

--- common/src/main/kotlin/org/valkyrienskies/mod/api/ValkyrienSkies.kt
+++ common/src/main/kotlin/org/valkyrienskies/mod/api/ValkyrienSkies.kt
@@ -6,15 +6,17 @@ import net.minecraft.world.level.ChunkPos
 import net.minecraft.world.level.Level
 import org.valkyrienskies.core.api.ships.LoadedShip
 import org.valkyrienskies.core.api.ships.Ship
+import org.valkyrienskies.mod.common.ValkyrienSkiesMod
 
-val vsApi: VsApi @JvmName("getApi") get() = TODO()
+val vsApi: VsApi @JvmName("getApi") get() = ValkyrienSkiesMod.api
 
 fun Level?.getShipManagingBlock(pos: BlockPos?) = vsApi.getShipManagingBlock(this, pos)
 fun Level?.getShipManagingBlock(x: Int, y: Int, z: Int) = getShipManagingChunk(x shr 4, z shr 4)
 
-fun Level?.getLoadedShipManagingBlock(pos: BlockPos?): LoadedShip? = TODO()
+fun Level?.getLoadedShipManagingBlock(pos: BlockPos?): LoadedShip? = getShipManagingBlock(pos) as? LoadedShip
 
-fun Level?.getDeadShipManagingBlock(pos: BlockPos?): Ship? = TODO()
+fun Level?.getDeadShipManagingBlock(pos: BlockPos?): Ship? =
+    getShipManagingBlock(pos)?.takeUnless { it is LoadedShip }
 
 fun Level?.getShipManagingChunk(pos: ChunkPos?) = vsApi.getShipManagingChunk(this, pos)
 fun Level?.getShipManagingChunk(chunkX: Int, chunkZ: Int) = vsApi.getShipManagingChunk(this, chunkX, chunkZ)
